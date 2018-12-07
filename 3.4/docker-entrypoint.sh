3909a944a35a5610cd026aeaa07932bc53007da7
Akai Z <akai-z@users.noreply.github.com>
2018-12-07 22:00:41 +0200

    Apply the official MongoDB Docker image updates to version 3.4 entrypoint script

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -12,7 +12,7 @@ originalArgOne="$1"
 # all mongo* commands should be dropped to the correct user
 if [[ "$originalArgOne" == mongo* ]] && [ "$(id -u)" = '0' ]; then
   if [ "$originalArgOne" = 'mongod' ]; then
-    chown -R mongodb /data/configdb /data/db
+    find /data/configdb /data/db \! -user mongodb -exec chown mongodb '{}' +
   fi
 
   # make sure we can write to stdout and stderr as "mongodb"
@@ -169,7 +169,7 @@ _dbPath() {
 
   if ! dbPath="$(_mongod_hack_get_arg_val --dbpath "$@")"; then
     if _parse_config "$@"; then
-      dbPath="$(jq -r '.storage.dbPath' "$jsonConfigFile")"
+      dbPath="$(jq -r '.storage.dbPath // empty' "$jsonConfigFile")"
     fi
   fi
 
